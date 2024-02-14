3b8ee2e70ac23d3aa1fa0b3d3482f6a69d8c78b3
seanc123e <seanc123e@yahoo.com>
2024-02-14 12:35:09 -0600

    finished practice. source code from briancodex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from '../src/components/Navbar';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +15,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' exact Component={Home}/>
+        <Route path='/services' Component={Services} />
+        <Route path='/products' Component={Products} />
+        <Route path='/sign-up' Component={SignUp} />
       </Routes>
       
     </Router>
